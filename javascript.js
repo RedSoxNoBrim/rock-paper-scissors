2409e4417d6ff21d36cf4c34ca252fbc13d91028
RedSoxNoBrim <dgarrett0720@gmail.com>
2026-08-26 09:00:38 -0400

    completed project

---
+++ javascript.js
@@ -0,0 +1,86 @@
+let humanScore = 0
+let computerScore = 0
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+function getComputerChoice() {
+    let answer = getRandomInt(3);
+    if (answer === 0) {
+        console.log("Rock")
+        return "Rock";
+    } else if (answer === 1) {
+        console.log("Paper")
+        return "Paper";
+    } else {
+        console.log("Scissors")
+        return "Scissors";
+    }
+}
+
+getComputerChoice();
+
+function getHumanChoice() {
+    answer = prompt("Choose rock, paper, or scissors. ")
+    answer.toLowerCase();
+    console.log(answer)
+    return answer;
+}
+
+
+
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === "rock" && computerChoice === "Paper") {
+        return "You lose!";
+    } else if (humanChoice === "paper" && computerChoice === "Scissors") {
+        return "You lose!";
+    } else if (humanChoice === "scissors" && computerChoice === "Rock") {
+        return "You lose!";
+    } else if (humanChoice === "rock" && computerChoice === "Scissors") {
+        return "You win!";
+    } else if (humanChoice === "paper" && computerChoice === "Rock") {
+        return "You win!";
+    } else if (humanChoice === "scissors" && computerChoice === "Paper") {
+        return "You win!";
+    } else if (humanChoice === "rock" && computerChoice === "Rock") {
+        return "It's a tie!";
+    } else if (humanChoice === "paper" && computerChoice === "Paper") {
+        return "It's a tie!";
+    } else if (humanChoice === "scissors" && computerChoice === "Scissors") {
+        return "It's a tie!";
+    }
+}
+
+
+
+
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let result = playRound(getHumanChoice(), getComputerChoice());
+
+        
+
+        if (result === "You win!") {
+            humanScore++
+        } else if (result === "You lose!") {
+            computerScore++
+        }
+
+        console.log("Human score:", humanScore)
+        console.log("Computer score:", computerScore)
+    }
+
+    
+
+
+}
+
+playGame();
+
